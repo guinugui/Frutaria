05e528d87cb29f5e942abc0a758297f72bf3442e
Guilherme Ribeiro <guilhermevendaseprodutos@gmail.com>
2024-02-18 10:26:55 -0300

    lista de frutas

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,45 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.frutas;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		List<frutas> list = new ArrayList<>();
+
+		System.out.println("quer fazer a lista de frutas:s/n");
+		char ch = sc.next().charAt(0);
+		
+		if(ch == 's') {
+			
+			System.out.println("quantas frutas deseja por: ");
+			int n = sc.nextInt();			
+			for (int i = 0; i <= n; i++) {
+				sc.nextLine();
+				System.out.println("nome da fruta: ");
+				String name = sc.nextLine();
+				System.out.println("quantidade da furta :");
+				Integer quantity = sc.nextInt();
+				
+				frutas fruta = new frutas(name, quantity);
+				list.add(fruta);
+			}
+			
+			
+		}
+		
+
+		for (frutas f : list) {
+			System.out.println(f.getName() + ", " + f.getQuantity());
+		}
 
 	}
-
 }
---
+++ src/entities/frutas.java
@@ -0,0 +1,40 @@
+package entities;
+
+public class frutas {
+
+	private String name;
+	private Integer quantity;
+	
+
+	
+	public frutas(String name, Integer quantity) {
+		this.name = name;
+		this.quantity = quantity;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	
+	@Override
+	public String toString() {
+		return name + ", " + quantity;
+	}
+	
+}
